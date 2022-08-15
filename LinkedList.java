c9add746b9ca43dfff0880e47524206a3208f412
8099751086 <dilwarhussainit24@gmail.com>
2022-08-15 21:36:36 +0530

    Add Elements

---
+++ LinkedList.java
@@ -0,0 +1,31 @@
+package com.linkedlist;
+/**
+* dilwar hussain
+
+
+*/
+
+public class LinkedList<T> {
+	Node<T> head;
+	Node<T> tail;
+
+	void push(T data) {
+		Node<T> node = new Node<>(data);
+		if (head == null) {
+			head = node;
+			tail = node;
+		} else {
+			node.next = head;
+			head = node;
+		}
+	}
+
+	void display() {
+		Node<T> temp = head;
+		while (temp != null) {
+			System.out.print(temp.data + " ");
+			temp = temp.next;
+		}
+	}
+
+}
---
+++ LinkedListMain.java
@@ -0,0 +1,13 @@
+package com.linkedlist;
+
+public class LinkedListMain {
+	public static void main(String[] args) {
+		LinkedList<Integer> linkedList = new LinkedList<>();
+		linkedList.push(70);
+		linkedList.push(30);
+		linkedList.push(56);
+
+		linkedList.display();
+
+	}
+}
